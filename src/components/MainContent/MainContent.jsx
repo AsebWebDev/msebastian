4842a33775cf12b77defc0e1b0ff00d3bddf531a
André Sebastian-Bath <a.sebastian@posteo.de>
2020-12-28 18:32:13 +0100

    add basic switch functionality

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom';
 import './MainContent.scss'
-import CV from './CV/CV'
+import AboutMe from './AboutMe/AboutMe'
   
   export default function MainContent() {
     return (
-        <div id="main"><CV /></div>
+      <div id="main">
+        <Switch>
+              {/* <Route path="/" exact component={isLoggedIn ? Dashboard : LandingPage} /> */}
+              <Route path="/aboutme" component={AboutMe} />
+              <Route render={() => <h2>404</h2>} />
+        </Switch>
+      </div>
     )
 }
