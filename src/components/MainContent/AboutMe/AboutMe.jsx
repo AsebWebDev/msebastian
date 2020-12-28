d9abadc30cc438b2439453a5205c1b02ac585739
André Sebastian-Bath <a.sebastian@posteo.de>
2020-12-28 18:32:27 +0100

    add aboutme component

--- src/components/MainContent/AboutMe/AboutMe.jsx
+++ src/components/MainContent/AboutMe/AboutMe.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Container, Header } from "semantic-ui-react";
 import cvText from '../../../texts/cv'
-import "./CV.scss";
+import "./AboutMe.scss";
 
 export default function CV() {
   return (
-    <Container text id="cv">
+    <Container text id="aboutme">
       <Header as='h1'>Curriculum Vitae</Header>
       <p>{cvText}</p>
     </Container>
