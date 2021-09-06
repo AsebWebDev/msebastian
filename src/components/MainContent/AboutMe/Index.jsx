c2ff17c5ff55df3baa24351d3e052c1fa19b2bdd
André Sebastian-Bath <a.sebastian@posteo.de>
2021-09-06 15:13:07 +0200

    add news section in about me

--- src/components/MainContent/AboutMe/Index.jsx
+++ src/components/MainContent/AboutMe/Index.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import { Grid } from "semantic-ui-react";
 import AboutMe from './AboutMe'
 import CV from './CV'
+import News from './News'
 import "./Index.scss";
 
 export default function Index() {
   return (
-    <Grid stackable columns={2} id="aboutme-index">
+    <Grid stackable columns={3} id="aboutme-index">
       <Grid.Row>
         <Grid.Column width={4}>
           <AboutMe />
         </Grid.Column>
-        <Grid.Column width={12}>
+        <Grid.Column width={6}>
           <CV />
+        </Grid.Column>
+        <Grid.Column width={4}>
+          <News />
         </Grid.Column>
       </Grid.Row>
     </Grid>
