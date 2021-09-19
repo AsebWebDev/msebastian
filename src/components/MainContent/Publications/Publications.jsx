b1f2074d710eb40a60bc9c5db967438b311d9279
André Sebastian-Bath <a.sebastian@posteo.de>
2021-09-19 10:21:19 +0200

    remove template literal syntax from ListItem props

--- src/components/MainContent/Publications/Publications.jsx
+++ src/components/MainContent/Publications/Publications.jsx
@@ -12,32 +12,32 @@ export default function Publications() {
 
         <Header color='blue' inverted as='h2'>{dict.de.PUBLICATIONS.PUBLICATIONS_1_HEADER}</Header>
         <List inverted divided relaxed >
-          {dict.de.PUBLICATIONS.PUBLICATIONS_1_ITEMS.map(publication => <ListItem header={`${publication.author}`} metaText={`${publication.title}`}/>)}
+          {dict.de.PUBLICATIONS.PUBLICATIONS_1_ITEMS.map(publication => <ListItem header={publication.author} metaText={publication.title}/>)}
         </List>
 
         {/* Artikel in Begutachtung */}
 
         <Header color='blue' inverted as='h2'>{dict.de.PUBLICATIONS.PUBLICATIONS_2_HEADER}</Header>
         <List inverted divided relaxed >
-          {dict.de.PUBLICATIONS.PUBLICATIONS_2_ITEMS.map(publication => <ListItem header={`${publication.author}`} metaText={`${publication.title}`}/>)}
+          {dict.de.PUBLICATIONS.PUBLICATIONS_2_ITEMS.map(publication => <ListItem header={publication.author} metaText={publication.title}/>)}
         </List>
 
         {/* Artikel in wissenschaftlichen Sammelbänden */}
         <Header color='blue' inverted as='h2'>{dict.de.PUBLICATIONS.PUBLICATIONS_3_HEADER}</Header>
         <List inverted divided relaxed>
-          {dict.de.PUBLICATIONS.PUBLICATIONS_3_ITEMS.map(publication => <ListItem header={`${publication.author}`} metaText={`${publication.title}`}/>)}
+          {dict.de.PUBLICATIONS.PUBLICATIONS_3_ITEMS.map(publication => <ListItem header={publication.author} metaText={publication.title}/>)}
         </List> 
           
         {/* Wissenschaftsjournalistische und andere Artikel */}
         <Header color='blue' inverted as='h2'>{dict.de.PUBLICATIONS.PUBLICATIONS_4_HEADER}</Header>
         <List inverted divided relaxed>
-          {dict.de.PUBLICATIONS.PUBLICATIONS_4_ITEMS.map(publication => <ListItem header={`${publication.author}`} metaText={`${publication.title}`}/>)}
+          {dict.de.PUBLICATIONS.PUBLICATIONS_4_ITEMS.map(publication => <ListItem header={publication.author} metaText={publication.title}/>)}
         </List> 
 
         {/* Tagungsberichte und Rezensionen */}
         <Header color='blue' inverted as='h2'>{dict.de.PUBLICATIONS.PUBLICATIONS_5_HEADER}</Header>
         <List inverted divided relaxed>
-          {dict.de.PUBLICATIONS.PUBLICATIONS_5_ITEMS.map(publication => <ListItem header={`${publication.author}`} metaText={`${publication.title}`}/>)}
+          {dict.de.PUBLICATIONS.PUBLICATIONS_5_ITEMS.map(publication => <ListItem header={publication.author} metaText={publication.title}/>)}
         </List> 
       </Container>
     )
