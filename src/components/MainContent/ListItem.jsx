8b136ebeb9f0d8b05c8893e5f99077d748c06f43
André Sebastian-Bath <a.sebastian@posteo.de>
2021-09-19 10:22:22 +0200

    add link property to ListItem

--- src/components/MainContent/ListItem.jsx
+++ src/components/MainContent/ListItem.jsx
@@ -1,16 +1,34 @@
 import { List } from "semantic-ui-react";
-import './ListItem.scss'
+import "./ListItem.scss";
 
-function ListItem({header, metaText, dark}) {
-    return (
-        <List.Item id="list-item">
-          <List.Icon size="mini" name='circle' verticalAlign='middle' />
-          <List.Content>
-            <List.Header as='a'>{header}</List.Header>
-            <List.Description as='a'className={dark ? '' : "list-description"}>{metaText}</List.Description>
-          </List.Content>
-        </List.Item>
-    )
+function ListItem({ header, link, metaText, dark }) {
+  return (
+    <List.Item id="list-item">
+      <List.Icon
+        size="mini"
+        name="circle"
+        verticalAlign="middle"
+      />
+      <List.Content>
+        <List.Header as="a">{header}</List.Header>
+        <List.Description
+          as="a"
+          className={dark ? "" : "list-description"}
+        >
+          {metaText}
+        </List.Description>
+        {link && (
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <strong>Link zur Publikation</strong>
+          </a>
+        )}
+      </List.Content>
+    </List.Item>
+  );
 }
 
-export default ListItem
+export default ListItem;
--- src/components/MainContent/Publications/Publications.jsx
+++ src/components/MainContent/Publications/Publications.jsx
@@ -1,44 +1,96 @@
-import { Container, Header, List } from 'semantic-ui-react';
-import ListItem from '../ListItem'
-import { dict } from '../../../utilities/translation'
-import './Publications.scss'
+import { Container, Header, List } from "semantic-ui-react";
+import ListItem from "../ListItem";
+import { dict } from "../../../utilities/translation";
+import "./Publications.scss";
 
 export default function Publications() {
-    return (
-      <Container text id='publications'>
-        <Header inverted as='h1'>{dict.de.PUBLICATIONS.PUBLICATIONS_TITLE}</Header>
-        
-        {/* Artikel in Fachzeitschriften */}
+  return (
+    <Container text id="publications">
+      <Header inverted as="h1">
+        {dict.de.PUBLICATIONS.PUBLICATIONS_TITLE}
+      </Header>
 
-        <Header color='blue' inverted as='h2'>{dict.de.PUBLICATIONS.PUBLICATIONS_1_HEADER}</Header>
-        <List inverted divided relaxed >
-          {dict.de.PUBLICATIONS.PUBLICATIONS_1_ITEMS.map(publication => <ListItem header={publication.author} metaText={publication.title}/>)}
-        </List>
+      {/* Artikel in Fachzeitschriften */}
 
-        {/* Artikel in Begutachtung */}
+      <Header color="blue" inverted as="h2">
+        {dict.de.PUBLICATIONS.PUBLICATIONS_1_HEADER}
+      </Header>
+      <List inverted divided relaxed>
+        {dict.de.PUBLICATIONS.PUBLICATIONS_1_ITEMS.map(
+          (publication) => (
+            <ListItem
+              header={publication.author}
+              link={publication.link}
+              metaText={publication.title}
+            />
+          )
+        )}
+      </List>
 
-        <Header color='blue' inverted as='h2'>{dict.de.PUBLICATIONS.PUBLICATIONS_2_HEADER}</Header>
-        <List inverted divided relaxed >
-          {dict.de.PUBLICATIONS.PUBLICATIONS_2_ITEMS.map(publication => <ListItem header={publication.author} metaText={publication.title}/>)}
-        </List>
+      {/* Artikel in Begutachtung */}
 
-        {/* Artikel in wissenschaftlichen Sammelbänden */}
-        <Header color='blue' inverted as='h2'>{dict.de.PUBLICATIONS.PUBLICATIONS_3_HEADER}</Header>
-        <List inverted divided relaxed>
-          {dict.de.PUBLICATIONS.PUBLICATIONS_3_ITEMS.map(publication => <ListItem header={publication.author} metaText={publication.title}/>)}
-        </List> 
-          
-        {/* Wissenschaftsjournalistische und andere Artikel */}
-        <Header color='blue' inverted as='h2'>{dict.de.PUBLICATIONS.PUBLICATIONS_4_HEADER}</Header>
-        <List inverted divided relaxed>
-          {dict.de.PUBLICATIONS.PUBLICATIONS_4_ITEMS.map(publication => <ListItem header={publication.author} metaText={publication.title}/>)}
-        </List> 
+      <Header color="blue" inverted as="h2">
+        {dict.de.PUBLICATIONS.PUBLICATIONS_2_HEADER}
+      </Header>
+      <List inverted divided relaxed>
+        {dict.de.PUBLICATIONS.PUBLICATIONS_2_ITEMS.map(
+          (publication) => (
+            <ListItem
+              header={publication.author}
+              link={publication.link}
+              metaText={publication.title}
+            />
+          )
+        )}
+      </List>
 
-        {/* Tagungsberichte und Rezensionen */}
-        <Header color='blue' inverted as='h2'>{dict.de.PUBLICATIONS.PUBLICATIONS_5_HEADER}</Header>
-        <List inverted divided relaxed>
-          {dict.de.PUBLICATIONS.PUBLICATIONS_5_ITEMS.map(publication => <ListItem header={publication.author} metaText={publication.title}/>)}
-        </List> 
-      </Container>
-    )
+      {/* Artikel in wissenschaftlichen Sammelbänden */}
+      <Header color="blue" inverted as="h2">
+        {dict.de.PUBLICATIONS.PUBLICATIONS_3_HEADER}
+      </Header>
+      <List inverted divided relaxed>
+        {dict.de.PUBLICATIONS.PUBLICATIONS_3_ITEMS.map(
+          (publication) => (
+            <ListItem
+              header={publication.author}
+              link={publication.link}
+              metaText={publication.title}
+            />
+          )
+        )}
+      </List>
+
+      {/* Wissenschaftsjournalistische und andere Artikel */}
+      <Header color="blue" inverted as="h2">
+        {dict.de.PUBLICATIONS.PUBLICATIONS_4_HEADER}
+      </Header>
+      <List inverted divided relaxed>
+        {dict.de.PUBLICATIONS.PUBLICATIONS_4_ITEMS.map(
+          (publication) => (
+            <ListItem
+              header={publication.author}
+              link={publication.link}
+              metaText={publication.title}
+            />
+          )
+        )}
+      </List>
+
+      {/* Tagungsberichte und Rezensionen */}
+      <Header color="blue" inverted as="h2">
+        {dict.de.PUBLICATIONS.PUBLICATIONS_5_HEADER}
+      </Header>
+      <List inverted divided relaxed>
+        {dict.de.PUBLICATIONS.PUBLICATIONS_5_ITEMS.map(
+          (publication) => (
+            <ListItem
+              header={publication.author}
+              link={publication.link}
+              metaText={publication.title}
+            />
+          )
+        )}
+      </List>
+    </Container>
+  );
 }
