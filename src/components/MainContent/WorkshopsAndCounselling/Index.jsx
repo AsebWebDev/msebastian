a018b30eb6dd21aacaba4c881ee5edf8fae72f8a
André Sebastian-Bath <andre.sebastian-bath@erasys.de>
2021-11-07 16:49:28 +0100

    comment out background picture for now

--- src/components/MainContent/WorkshopsAndCounselling/Index.jsx
+++ src/components/MainContent/WorkshopsAndCounselling/Index.jsx
@@ -1,18 +1,19 @@
 import React from "react";
-import { Grid } from "semantic-ui-react";
+// TODO: Re-Introduce SCSS, when background image is ready
+// import { Grid } from "semantic-ui-react";
 import WorkshopsAndCounselling from './WorkshopsAndCounselling'
 import './Index.scss'
 
 export default function Index() {
   return (
-        <Grid stackable columns={3} id="wc-index">
-            <Grid.Row>
-            {/* <Grid.Column width={5}/>
-            <Grid.Column width={10}> */}
+        // <Grid stackable columns={3} id="wc-index">
+        //     <Grid.Row>
+        //     <Grid.Column width={5}/>
+        //     <Grid.Column width={10}>
                 <WorkshopsAndCounselling />
-            {/* </Grid.Column>
-            <Grid.Column width={1} /> */}
-            </Grid.Row>
-        </Grid>
+        //     </Grid.Column>
+        //     <Grid.Column width={1} />
+        //     </Grid.Row>
+        // </Grid>
   );
 }
