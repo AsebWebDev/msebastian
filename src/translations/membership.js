d83af1a8a025c666dc60c6f7fd912b295b3b6cc3
DrMarSeb <89970204+DrMarSeb@users.noreply.github.com>
2021-11-15 08:25:42 +0100

    211115 Typo Membership
    Typo entfernt

--- src/translations/membership.js
+++ src/translations/membership.js
@@ -7,34 +7,34 @@ const membership = {
                 title: `Transformationskolleg des Norbert-Elias-Center, Europa Universität Flensburg`,
               },
               {
-                title: `Group for Society and Animals Studies (GSA), Universität Hamburg}`,
+                title: `Group for Society and Animals Studies, Universität Hamburg}`,
               },
               {
-                title: `Deutsche Gesellschaft für Soziologie (DGS) und DGS-Sektion Land-, Agrar- und Ernährungssoziologie sowie Arbeitsgemeinschaft Gewaltsoziologie in der DGS`,
+                title: `Deutsche Gesellschaft für Soziologie und DGS-Sektion Land-, Agrar- und Ernährungssoziologie sowie Arbeitsgemeinschaft Gewaltsoziologie in der DGS`,
               },
               { title: `Minding Animals International` },
               {
-                title: `Interdisciplinary Centre for European Studies (ICES), Europa Universität Flensburg`,
+                title: `Interdisciplinary Centre for European Studies, Europa Universität Flensburg`,
               },
             ],
           },
     },
     en: {
         MEMBERSHIP: {
-            MEMBERSHIP_TITLE: `embership in scientific Organizations`,
+            MEMBERSHIP_TITLE: `Membership in scientific Organizations`,
             ITEMS: [
               {
                 title: `Transformationskolleg at the Norbert-Elias-Center, Europa Universität Flensburg`,
               },
               {
-                title: `Group for Society and Animals Studies (GSA), University of Hamburg}`,
+                title: `Group for Society and Animals Studies, University of Hamburg}`,
               },
               {
-                title: `German Sociological Association, including Section "Land-, Agrar- und Ernährungssoziologie" as well as working group Sociology of Violence`,
+                title: `German Sociological Association, including Section "Land-, Agrar- und Ernährungssoziologie" as well as working group "Sociology of Violence"`,
               },
               { title: `Minding Animals International` },
               {
-                title: `Interdisciplinary Centre for European Studies (ICES), Europa Universität Flensburg`,
+                title: `Interdisciplinary Centre for European Studies, Europa Universität Flensburg`,
               },
             ],
           },
