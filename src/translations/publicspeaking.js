0c9cd64757aca89aa849f83fc27ed97f80b8ac73
DrMarSeb <89970204+DrMarSeb@users.noreply.github.com>
2021-11-15 08:54:26 +0100

    211115 en translation added
    Added translations

--- src/translations/publicspeaking.js
+++ src/translations/publicspeaking.js
@@ -47,43 +47,43 @@ const publicspeaking = {
   en: {
     PUBLICSPEAKING: {
       PUBLICSPEAKING_TITLE: `Public Speaking`,
-      PUBLICSPEAKING_1_HEADER: `Sie können mich als Referenten für einen Vortrag einladen. Die Liste meiner Vorträge auf Einladung zivilgesellschaftlicher und politischer Organisationen gibt Ihnen einen Eindruck möglicher Themen. Natürlich können auch individuelle Schwerpunkte besprochen werden.`,
+      PUBLICSPEAKING_1_HEADER: `You can invite me as a speaker for a lecture. The list of my lectures on invitation of civil society and political organizations gives you an impression of possible topics. Of course, individual focal points can also be discussed.`,
       PUBLICSPEAKING_1_ITEMS: [
         {
           title: `10.03.2021, Berlin,`,
-          text: `„Mensch und Tier in der Krise – Ambivalenzen im gesellschaftlichen Mensch-Tier-Verhältnis“, im Rahmen des Berliner Tierschutzforum der Landestierschutzbeauftragten des Landes Berlin`,
+          text: `„Mensch und Tier in der Krise – Ambivalenzen im gesellschaftlichen Mensch-Tier-Verhältnis“ ["Humans and Animals in Crisis - Ambivalences in the Social Human-Animal Relationship"], as part of the Berlin Animal Welfare Forum of the Animal Welfare Officers of the State of Berlin`,
         },
         {
           title: `13.01.2020, Hamburg`,
-          text: `„Soziologie des Schlachthofs. Wie Schlachthofarbeiter mit ihrer Arbeit umgehen.“, im Rahmen der Vortragsreihe „Komplex Tier“, Werkstatt 3`,
+          text: `„Soziologie des Schlachthofs. Wie Schlachthofarbeiter mit ihrer Arbeit umgehen“ ["Sociology of the slaughterhouse. How slaughterhouse workers cope with their work"], as part of the lecture series „Komplex Tier“, Werkstatt 3`,
         },
         {
           title: `11.09.2017, Hamburg`,
-          text: `Teilnahme als Experte an öffentlichem Interview und Publikumsgespräch anlässlich der Premiere des Dokumentarfilms ‘The End of Meat’, Abaton Kino, Hamburg`,
+          text: `Participation during a public interview and discussion during the premiere of the documentary film  ‘The End of Meat’, Abaton Kino`,
         },
         {
           title: `26.8.2017, Berlin`,
-          text: `Teilnahme als Experte an Podiumsdiskussion „The End of Meat - perspectives of change: The role of flexitarians, cultured meat and economic reasons“ im Rahmen der Konferenz „The End of Meat“`,
+          text: `Participation at the panel debate „The End of Meat - perspectives of change: The role of flexitarians, cultured meat and economic reasons“ during the conference „The End of Meat“`,
         },
         {
           title: `4.11.2016, Berlin`,
-          text: `„Ambivalente Verhältnisse: Das Mensch-Tier-Verhältnis aus soziologischer Perspektive“, invited Lecture im Rahmen des Symposiums „Networking Animal Lovers“ der Neuen Gesellschaft für bildende Kunst`,
+          text: `„Ambivalente Verhältnisse: Das Mensch-Tier-Verhältnis aus soziologischer Perspektive“ ["Ambivalent Relationships: The Human-Animal Relations from a Sociological Perspective"], invited Lecture during the Symposium „Networking Animal Lovers“ of the Neue Gesellschaft für bildende Kunst`,
         },
         {
           title: `5.8.2016, Tübingen`,
-          text: `„Warum essen wir die einen und streicheln die anderen? Ambivalenzen im Mensch-Tier-Verhältnis“, invited Lecture im Rahmenprogramm der Ausstellung „Haustiere – lieb und lecker“, Stadtmuseum Tübingen`,
+          text: `„Warum essen wir die einen und streicheln die anderen? Ambivalenzen im Mensch-Tier-Verhältnis" ["Why do we eat some and pet others? Ambivalences in the human-animal relationship"], invited Lecture in the program of the exhibition „Haustiere – lieb und lecker“, Stadtmuseum Tübingen`,
         },
         {
           title: `4.8.2016, Fellbach`,
-          text: `„Warum essen wir die einen und streicheln die anderen? Ambivalenzen im Mensch-Tier-Verhältnis“, invited Lecture im Rahmenprogramm der 13. Triennale Kleinplastik, Rathaus Fellbach`,
+          text: `„Warum essen wir die einen und streicheln die anderen? Ambivalenzen im Mensch-Tier-Verhältnis“ ["Why do we eat some and pet others? Ambivalences in the human-animal relationship"] , invited Lecture in the program of the 13th Triennale Kleinplastik, Rathaus Fellbach`,
         },
         {
           title: `21.2.2015, Wiesbaden`,
-          text: `Teilnahme als Experte an Podiumsdiskussion „Perspektiven zur Überwindung von Tierausbeutung. Möglichkeiten parlamentarischer und außerparlamentarischer Initiativen“ im Rahmen der Konferenz „Tiere sind keine Ware“ der Bundestagsfraktion und der Fraktion im Hessischen Landtag der Linkspartei`,
+          text: `Participation at the panel debate „Perspektiven zur Überwindung von Tierausbeutung. Möglichkeiten parlamentarischer und außerparlamentarischer Initiativen“ ["Perspectives on overcoming animal exploitation. Possibilities of parliamentary and extra-parliamentary initiatives"] during the conference „Tiere sind keine Ware“ of the Bundestag Fraction and the Fraction in the Hessian State Parliament of the Linkspartei`,
         },
         {
           title: `19.10.2013, Wien`,
-          text: `"Was ist Gewalt an Tieren und wie gehen Menschen mit ihr um? Das Mensch-Tier-Verhältnis aus Sicht der soziologischen Gewaltforschung", Depot Wien`,
+          text: `"Was ist Gewalt an Tieren und wie gehen Menschen mit ihr um? Das Mensch-Tier-Verhältnis aus Sicht der soziologischen Gewaltforschung" ["What is violence against animals and how do humans deal with it? The human-animal relationship from the perspective of sociological research on violence"], Depot Wien`,
         },
       ],
     },
