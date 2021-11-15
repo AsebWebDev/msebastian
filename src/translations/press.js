db2f3d61323defc3113801216dbfdbd6026c7de7
DrMarSeb <89970204+DrMarSeb@users.noreply.github.com>
2021-11-15 08:36:36 +0100

    211115 Media new text
    Changed the text

--- src/translations/press.js
+++ src/translations/press.js
@@ -3,8 +3,8 @@ const press = {
         PRESS: {
             PRESS_TITLE: `Presse`,
             PRESS_MODAL_TITLE: `Presse-PDF`,
-            PRESS_MODAL_HEADER: `Liste an Presseerwähnungen`,
-            PRESS_MODAL_DESCRIPTION: `Diese Datei enthält eine Liste der Presseerwähnungen von Dr. Marcel Sebastian.`,
+            PRESS_MODAL_HEADER: `Liste meiner Medienauftitte`,
+            PRESS_MODAL_DESCRIPTION: `Diese Datei enthält eine Liste der Medienauftritte von Dr. Marcel Sebastian.`,
             PRESS_MODAL_QUESTION: `Möchten Sie diese Datei herunterladen? `,
             PRESS_MODAL_CONFIRM: `Ja, herunterladen`,
             PRESS_MODAL_DENY: `Nein`,
@@ -14,7 +14,7 @@ const press = {
         PRESS: {
             PRESS_TITLE: `Presse`,
             PRESS_MODAL_TITLE: `Presse-PDF`,
-            PRESS_MODAL_HEADER: `Me in the media`,
+            PRESS_MODAL_HEADER: `Collection of my media appearances`,
             PRESS_MODAL_DESCRIPTION: `This file contains a list of media appearances by Dr. Marcel Sebastian.`,
             PRESS_MODAL_QUESTION: `Do you want to download this file? `,
             PRESS_MODAL_CONFIRM: `Yes, download`,
