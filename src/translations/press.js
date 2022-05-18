3d5e2d7cf7f0648d7bee7e917ce23e23108e84e6
André Sebastian-Bath <a.sebastian@posteo.de>
2022-05-18 17:40:52 +0200

    fix funny typo

--- src/translations/press.js
+++ src/translations/press.js
@@ -1,27 +1,26 @@
 const press = {
-    de: {
-        PRESS: {
-            PRESS_TITLE: `Presse`,
-            PRESS_MODAL_TITLE: `Presse-PDF`,
-            PRESS_MODAL_HEADER: `Liste meiner Medienauftitte`,
-            PRESS_MODAL_DESCRIPTION: `Diese Datei enthält eine Liste der Medienauftritte von Dr. Marcel Sebastian.`,
-            PRESS_MODAL_QUESTION: `Möchten Sie diese Datei herunterladen? `,
-            PRESS_MODAL_CONFIRM: `Ja, herunterladen`,
-            PRESS_MODAL_DENY: `Nein`,
-          },
+  de: {
+    PRESS: {
+      PRESS_TITLE: `Presse`,
+      PRESS_MODAL_TITLE: `Presse-PDF`,
+      PRESS_MODAL_HEADER: `Liste meiner Medienauftritte`,
+      PRESS_MODAL_DESCRIPTION: `Diese Datei enthält eine Liste der Medienauftritte von Dr. Marcel Sebastian.`,
+      PRESS_MODAL_QUESTION: `Möchten Sie diese Datei herunterladen? `,
+      PRESS_MODAL_CONFIRM: `Ja, herunterladen`,
+      PRESS_MODAL_DENY: `Nein`,
     },
-    en: {
-        PRESS: {
-            PRESS_TITLE: `Media`,
-            PRESS_MODAL_TITLE: `Media-PDF`,
-            PRESS_MODAL_HEADER: `Collection of my media appearances`,
-            PRESS_MODAL_DESCRIPTION: `This file contains a list of media appearances by Dr. Marcel Sebastian.`,
-            PRESS_MODAL_QUESTION: `Do you want to download this file? `,
-            PRESS_MODAL_CONFIRM: `Yes, download`,
-            PRESS_MODAL_DENY: `No`,
-          },
+  },
+  en: {
+    PRESS: {
+      PRESS_TITLE: `Media`,
+      PRESS_MODAL_TITLE: `Media-PDF`,
+      PRESS_MODAL_HEADER: `Collection of my media appearances`,
+      PRESS_MODAL_DESCRIPTION: `This file contains a list of media appearances by Dr. Marcel Sebastian.`,
+      PRESS_MODAL_QUESTION: `Do you want to download this file? `,
+      PRESS_MODAL_CONFIRM: `Yes, download`,
+      PRESS_MODAL_DENY: `No`,
     },
-  };
-  
-  export default press;
-  
+  },
+};
+
+export default press;
