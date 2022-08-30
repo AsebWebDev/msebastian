3e3806ab7f8169e101b6ef6bcde0260ae884e621
DrMarSeb <89970204+DrMarSeb@users.noreply.github.com>
2022-08-30 18:10:46 +0200

    Update aboutme.js

--- src/translations/aboutme.js
+++ src/translations/aboutme.js
@@ -4,7 +4,7 @@ const aboutme = {
             ABOUT_ME_TITLE: `Über mich`,
             ABOUT_ME_SUMMARY: `In meiner Forschung analysiere ich den Zusammenhang von Institutionenwandel, Kulturwandel und Akteurshandeln. Mein
                           thematischer Schwerpunkt ist die Soziologie der Mensch-Tier-Beziehung. Dieses Thema untersuche ich unter anderem
-                          aus der Perspektive des historischen Institutionalismus, der Arbeitssoziologie, der Emotionssoziologie, der
+                          aus der Perspektive des historischen Institutionalismus, der Umweltsoziologie, Arbeitssoziologie, der politischen Soziologie, der
                           Gewaltsoziologie und der sozialen Bewegungsforschung.
                           Zudem bin ich als freier wissenschaftlicher Autor tätig und biete wissenschaftliche Beratung, Workshops sowie Vorträge an.`
           },
@@ -12,9 +12,9 @@ const aboutme = {
     en: {
         ABOUT_ME: {
             ABOUT_ME_TITLE: `About me`,
-            ABOUT_ME_SUMMARY: `In my research I analyze the retation between institutional change, cultural change and the influence of social actors in this process. 
-                          My thematic focus is the sociology of human-animal relations. I analyze this topic using theoretical concepts from historical new institutionalism,
-                          the Sociology of work, the sociology of emotions, the sociology of violence and social movement studies.
+            ABOUT_ME_SUMMARY: `In my research I analyze the relation between institutional change, cultural change and the influence of social actors in this process. 
+                          My thematic focus is the sociology of human-animal relations. I analyze this topic using theoretical concepts from historical institutionalism,
+                          environmental sociology, the sociology of work, political sociology, the sociology of violence and social movement studies.
                           I am also an independent academic author. You can book me for scientific advisory and lectures.`,
           },
     },
