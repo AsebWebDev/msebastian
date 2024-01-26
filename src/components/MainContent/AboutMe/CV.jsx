8f5237ed09dd856b6b5ad58a435d6005c2fd422f
André Sebastian-Bath <a.sebastian@posteo.de>
2024-01-26 12:32:03 +0100

    restructure About Me

--- src/components/MainContent/AboutMe/CV.jsx
+++ src/components/MainContent/AboutMe/CV.jsx
@@ -23,29 +23,6 @@ export default function CV() {
           />
         ))}
       </List>
-      <Header color="blue" as="h2">
-        {dict[lang].MEMBERSHIP.MEMBERSHIP_TITLE}
-      </Header>
-      <List>
-        {dict[lang].MEMBERSHIP.ITEMS.map((membership, i) => (
-          <ListItem
-            key={i} // TODO: add proper key to avoid using index
-            header={`${membership.title}`}
-          />
-        ))}
-      </List>
-
-      <Header color="blue" as="h2">
-        {dict[lang].EXPERT_OPINION.EXPERT_OPINION_TITLE}
-      </Header>
-      <List>
-        {dict[lang].EXPERT_OPINION.ITEMS.map((expertOpinion, i) => (
-          <ListItem
-            key={i} // TODO: add proper key to avoid using index
-            header={`${expertOpinion.title}`}
-          />
-        ))}
-      </List>
     </div>
   );
 }
